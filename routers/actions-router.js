ffab90b6f22cec274a2a44996c87746929b80de8
samjeffcoat <samjeffcoat@gmail.com>
2019-02-22 12:37:14 -0600

    Adds post method and it works

---
+++ routers/actions-router.js
@@ -0,0 +1,78 @@
+const express = require("express");
+
+const db = require("../data/helpers/actionModel");
+
+const router = express.Router();
+
+//get actions
+router.get("/", async (req, res) => {
+  try {
+    let actions = await db.get();
+    res.status(200).json(actions);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "The actions information could not be retrieved." });
+  }
+});
+
+// get actions by id
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await db.getById(req.params.id);
+    if (post.length > 0) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The action with that id does not exist" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "The action information could not be retrieved" });
+  }
+});
+
+// post actions
+// project_id us required
+/// project description is required, cannot be over 128 characters
+/// notes required, no length  limit
+
+router.post("/", async (req, res) => {
+  if (!req.body.project_id) {
+    res
+      .status(400)
+      .json({ error: " Please provide a project ID for this action" });
+    return;
+  }
+  if (!req.body.description) {
+    res
+      .status(400)
+      .json({ error: "Please provide a description for that action" });
+    return;
+  }
+  if (!req.body.description.length > 128) {
+    res.status(400).json({ error: "Description must be less than 128 chars" });
+    return;
+  }
+  if (!req.body.notes) {
+    res.status(400).json({ error: "Please provide notes for the action." });
+    return;
+  }
+  try {
+    const newAction = {
+      project_id: req.body.project_id,
+      description: req.body.description,
+      notes: req.body.notes
+    };
+    let insAction = await db.insert(newAction);
+    let theAction = await db.getById(insAction.id);
+    res.status(201).json(theAction);
+  } catch (error) {
+    res.status(500).json({ error: "Added" });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const projectsRouter = require("./routers/projects-router");
-
+const actionsRouter = require("./routers/actions-router");
 const server = express();
 
 server.use(express.json());
 server.use("/projects/", projectsRouter);
+server.use("/actions/", actionsRouter);
 server.get("/", (req, res) => {
   res.send(
     `
