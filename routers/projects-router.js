460af71dcbd93cc6071e95a4aa2da20229cefbae
samjeffcoat <samjeffcoat@gmail.com>
2019-02-22 11:46:24 -0600

    Adds ability to update by id, success

--- routers/projects-router.js
+++ routers/projects-router.js
@@ -76,4 +76,23 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+//updates the project.
+
+router.put("/:id", async (req, res) => {
+  if (!req.body.name) {
+    res.status(400).json({ error: " Please provide a name for the project" });
+    return;
+  }
+  try {
+    const project = await db.update(req.params.id, req.body);
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res.status(404).json({ message: "The project could not be found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error updating the project" });
+  }
+});
 module.exports = router;
