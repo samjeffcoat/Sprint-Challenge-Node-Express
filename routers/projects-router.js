957db562905c65d71e496b0a7cd5a1f6ca342f95
samjeffcoat <samjeffcoat@gmail.com>
2019-02-22 11:05:23 -0600

    Adds get project by Id, however its not working

--- routers/projects-router.js
+++ routers/projects-router.js
@@ -4,8 +4,6 @@ const db = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
-module.exports = router;
-
 // get projects
 
 router.get("/", async (req, res) => {
@@ -18,3 +16,25 @@ router.get("/", async (req, res) => {
     });
   }
 });
+
+//Get projects by ID
+
+router.get("/:id", async (req, res) => {
+  try {
+    const project = await db.getById(req.params.id);
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The project with that id cannot be found!" });
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "That project information cannot be found!" });
+  }
+});
+
+module.exports = router;
