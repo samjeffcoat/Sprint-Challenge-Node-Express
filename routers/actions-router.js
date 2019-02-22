582335bf44b0224455bef43f8c10c27b244f0ca8
samjeffcoat <samjeffcoat@gmail.com>
2019-02-22 12:42:48 -0600

    Adds the ability to delete an action, successfully deleted in postman

--- routers/actions-router.js
+++ routers/actions-router.js
@@ -75,4 +75,22 @@ router.post("/", async (req, res) => {
   }
 });
 
+// delete a action
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const count = await db.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: "That action has been nuked!" });
+    } else {
+      res.status(404).json({ message: "That action cannot be found!" });
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "There was an error removing that action" });
+  }
+});
+
 module.exports = router;
