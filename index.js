e9b5981ad3340202066ef92d7efeea3d71dcfd5f
samjeffcoat <samjeffcoat@gmail.com>
2019-02-22 10:28:45 -0600

    Sets up server file and index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+require ("dotenv").config();
+
+const server = require("./server.js");
+
+const port = process.env.PORT || 9999;
+
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port}***\n`);
+})
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require(express);
+
+const server= express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.send(
+        `
+        <h2> Sam's Beautiful API</h>
+        `
+    );
+})
+
+module.exports= server;
